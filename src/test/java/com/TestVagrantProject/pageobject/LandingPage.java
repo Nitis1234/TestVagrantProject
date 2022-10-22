2ae3d564521a1d6be68a6fc5aac754957d62d832
Nitis1234 <niriskuamrrout@yahoo.com>
2022-10-22 13:35:01 +0530

    New File

---
+++ src/test/java/com/TestVagrantProject/pageobject/LandingPage.java
@@ -0,0 +1,33 @@
+package com.TestVagrantProject.pageobject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LandingPage {
+	
+	WebDriver ldriver;
+
+	//2. Create constructor
+	public LandingPage(WebDriver rdriver){
+		ldriver = rdriver;
+		PageFactory.initElements(rdriver, this);//driver that will be used to lookup the web element
+
+	}
+	
+	@FindBy(xpath="//td[normalize-space()='India']")
+	WebElement Country;
+	
+	@FindBy(xpath="//tbody/tr[12]/td[1]/div[1]/ul[1]/li[1]")
+	WebElement ReleseDate;
+	
+	public String getCountryName() {
+		return (Country.getText());
+	}
+	
+	public String getReleseDate() {
+		return(ReleseDate.getText());
+	}
+
+}
---
+++ src/test/java/com/TestVagrantProject/testcases/ValidateReleseDateAndCountry.java
@@ -0,0 +1,49 @@
+package com.TestVagrantProject.testcases;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import com.TestVagrantProject.pageobject.LandingPage;
+import com.TestVagrantProject.utilities.BaseClas;
+@Listeners(com.TestVagrantProject.utilities.ExtentListinerClass.class)
+public class ValidateReleseDateAndCountry extends BaseClas {
+	@Test(priority=1)
+	public void ValidateCountry() throws IOException {
+		driver.get(url);
+		Log.info("Web Page Is Open");
+		LandingPage lp=new LandingPage(driver);
+		String Country=lp.getCountryName();
+		System.out.println(Country);
+		Log.info("Country Name iS Printed");
+		if(Country.equalsIgnoreCase("India")) {
+			Assert.assertTrue(true);
+			Log.info("Country Name Matched");
+		}else {
+			CaptureScreenshot(driver,"ValidateCountryName");
+			Log.info("Country Name Did nor Match");
+			Assert.assertTrue(false);
+		}
+	}
+	@Test(priority=2)
+	public void ValidateReleseDate() throws IOException {
+		driver.get(url);
+		Log.info("Web Page Is Open");
+		LandingPage lp=new LandingPage(driver);
+		String ReleseDate=lp.getReleseDate();
+		System.out.println(ReleseDate);
+		Log.info("ReleseDate iS Printed");
+		if(ReleseDate.equalsIgnoreCase("17 December 2021")) {
+			Assert.assertTrue(true);
+			Log.info("ReleseDate Matched");
+		}else {
+			CaptureScreenshot(driver,"ValidateCountryName");
+			Log.warn("ReleseDate Did nor Match");
+			Assert.assertTrue(false);
+		}
+	}
+	
+
+}
